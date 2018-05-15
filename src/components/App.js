3c30963e230d2916cc0522cb9110e82a0590cabb
jonroby <joncaroby@gmail.com>
2018-05-14 21:33:57 -0700

    test comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import User from './user/user';
+import TestComp from './testComp/TestComp';
 
 import './App.css';
 
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <User />
+        <TestComp />
       </div>
     );
   }
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,5 +1,6 @@
 import { all, call } from 'redux-saga/effects';
 import userSaga from '../../components/user/user-saga';
+import testCompSaga from '../../components/testComp/test-comp-saga';
 
 export default function* rootSaga() {
   yield all([
