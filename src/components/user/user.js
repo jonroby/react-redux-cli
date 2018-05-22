d761413e4eb3863b0136b148c620015ed3cf09fb
jonroby <joncaroby@gmail.com>
2018-05-21 19:59:24 -0700

    change imports to redux folder

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from './user-actions.js';
+import { fetchUser } from '../../redux/actions/user';
 
 class User extends Component {
   constructor(props) {
