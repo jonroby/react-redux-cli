22a7c6e2a9c4e6819570316bdb2eef726c68147c
jonroby <joncaroby@gmail.com>
2018-06-15 16:36:13 -0700

    impure actions

--- cli/generateFileStrings/actions.js
+++ cli/generateFileStrings/actions.js
@@ -1,13 +1,26 @@
 const { toConstant } = require('../helpers/convertVariables');
 
-const action = (d) => (
-`export const types = {
-  ${toConstant(d.action)}: "${toConstant(d.action)}"
+const action = (d) => {
+  const types =
+        d.actionConstants
+        .map((a, i) => i === d.actionConstants.length-1 ? `  ${a}: "${a}",` : `  ${a}: "${a}",\n`)
+        .reduce((acc, curr) => acc.concat(curr), '');
+
+  const exps =
+        d.actionConstants
+        .map((a, i) => [a, d.actionCamels[i]])
+        .map(a => {
+          return `export const ${a[1]} = payload => ({
+  type: types.${a[0]},
+  payload
+});\n`})
+        .join('');
+
+  return `export const types = {
+${types}
 };
 
-export const ${d.action} = payload => ({
-  type: types.${toConstant(d.action)},
-  payload
-});\n`);
+${exps}`;
+}
 
 module.exports = action;
